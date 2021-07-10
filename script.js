15956835fb47e4a2bdb86eddbbbe5639dcbe09e0
Idris Olubisi <heedris2olubisi@gmail.com>
2021-07-10 01:24:59 +0100

    All file uploaded

---
+++ script.js
@@ -0,0 +1,29 @@
+// Start Cam function
+const startCam = () => {
+    //Initialize video
+    const video = document.getElementById("video");
+
+    // validate video element
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+            .getUserMedia({ video: true })
+            .then((stream) => {
+                video.srcObject = stream;
+            })
+            .catch(function(error) {
+                console.log("Something went wrong!");
+            });
+    }
+};
+
+// Stop the webcam function
+const stopCam = () => {
+    let stream = video.srcObject;
+    let tracks = stream.getTracks();
+    tracks.forEach((track) => track.stop());
+    video.srcObject = null;
+};
+
+$(() => {
+    startCam();
+});
