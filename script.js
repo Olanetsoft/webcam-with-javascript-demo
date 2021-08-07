d779dc0af16312e8381a31ffed90620e7cfedbd7
Idris Olubisi <heedris2olubisi@gmail.com>
2021-08-07 12:48:43 +0100

    updated changes

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // Start Cam function
 const startCam = () => {
-    //Initialize video
-    const video = document.getElementById("video");
+  //Initialize video
+  const video = document.getElementById("video");
 
-    // validate video element
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices
-            .getUserMedia({ video: true })
-            .then((stream) => {
-                video.srcObject = stream;
-            })
-            .catch(function(error) {
-                console.log("Something went wrong!");
-            });
-    }
+  // validate video element
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        video.srcObject = stream;
+      })
+      .catch(function (error) {
+        console.log("Something went wrong!");
+      });
+  }
 };
 
 // Stop the webcam function
 const stopCam = () => {
-    let stream = video.srcObject;
-    let tracks = stream.getTracks();
-    tracks.forEach((track) => track.stop());
-    video.srcObject = null;
+  let stream = video.srcObject;
+  let tracks = stream.getTracks();
+  tracks.forEach((track) => track.stop());
+  video.srcObject = null;
 };
 
 $(() => {
-    startCam();
+  startCam();
 });
